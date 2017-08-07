aab0cb7fc8368345f7a3ef4f238177ab75fd7e9b
DerOli82 <DerOli82@alaoli.de>
2017-08-07 10:08:32 +0200

    Simple mouse & keyboard listener

---
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/common/ui/util/KeyboardListener.java
@@ -0,0 +1,6 @@
+package de.alaoli.games.minecraft.mods.lib.common.ui.util;
+
+public interface KeyboardListener 
+{
+	public void keyTyped( char typedChar, int keyCode ); 
+}
---
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/common/ui/util/MouseListener.java
@@ -0,0 +1,8 @@
+package de.alaoli.games.minecraft.mods.lib.common.ui.util;
+
+import java.io.IOException;
+
+public interface MouseListener 
+{
+	public void mouseClicked( int mouseX, int mouseY, int mouseButton ); 
+}
