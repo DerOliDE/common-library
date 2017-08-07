42c8733beca1b3881e063f66449c0a25b7e5d1e7
DerOli82 <DerOli82@alaoli.de>
2017-08-07 10:09:29 +0200

    cleanup

--- src/main/java/de/alaoli/games/minecraft/mods/lib/common/ui/util/MouseListener.java
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/common/ui/util/MouseListener.java
@@ -1,7 +1,5 @@
 package de.alaoli.games.minecraft.mods.lib.common.ui.util;
 
-import java.io.IOException;
-
 public interface MouseListener 
 {
 	public void mouseClicked( int mouseX, int mouseY, int mouseButton ); 
