ba14c137d3b5268ea536fbcb4f52428b731d6cfa
DerOli82 <DerOli82@alaoli.de>
2017-09-04 19:44:45 +0200

    rework composite pattern interface

--- src/main/java/de/alaoli/games/minecraft/mods/lib/common/util/Composite.java
+++ src/main/java/de/alaoli/games/minecraft/mods/lib/common/util/Composite.java
@@ -1,11 +1,23 @@
 package de.alaoli.games.minecraft.mods.lib.common.util;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public interface Composite<T extends Component> extends Component
 {
 	public void addComponent( T component );
+	public void addComponents( Collection<T> components );
+	default public void addComponents( T[] components )
+	{
+		this.addComponents( Arrays.asList( components ) );
+	}
+	
 	public void removeComponent( T component );
+	public void removeComponents( Collection<T> components );
+	default public void removeComponents( T[] components )
+	{
+		this.removeComponents( Arrays.asList( components ) );
+	}
 	
 	public boolean hasComponents();
 	public boolean existsComponent( T component );
